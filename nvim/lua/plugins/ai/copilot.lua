141b568a9bad45cbeaa289924e32ea9d03dac412
Krzysztof Garbowski <krzysztof.garbowski@appwise.pl>
2025-11-07 23:27:40 +0100

    nvim - updated config to only use one plugin for copilot

--- nvim/lua/plugins/ai/copilot.lua
+++ nvim/lua/plugins/ai/copilot.lua
@@ -1,3 +1,36 @@
 return {
-  'github/copilot.vim',
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  config = function()
+    require('copilot').setup {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = '<M-l>',
+          accept_word = false,
+          accept_line = false,
+          next = '<M-]>',
+          prev = '<M-[>',
+          dismiss = '<C-]>',
+        },
+      },
+      panel = {
+        enabled = true,
+        auto_refresh = false,
+      },
+      filetypes = {
+        yaml = false,
+        markdown = false,
+        help = false,
+        gitcommit = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ['.'] = false,
+      },
+    }
+  end,
 }
