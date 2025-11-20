95776bfad21541a2bb38b8f522f96ce0e5f50232
Krzysztof Garbowski <krzysztof.garbowski@appwise.pl>
2025-11-20 08:31:22 +0100

    fish - added new functions

---
+++ fish/functions/edir.fish
@@ -0,0 +1,3 @@
+function edir
+    eza -1 -l -g --total-size --time-style long-iso --git-ignore --ignore-glob 'cache|vendor|node_modules'
+end
---
+++ fish/functions/etree.fish
@@ -0,0 +1,3 @@
+function etree
+    eza -T -l -g --time-style long-iso --git-ignore --ignore-glob 'cache|vendor|node_modules'
+end
